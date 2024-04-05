fdf51cc4a79f18fa55979b053d8c6980f4ccf250
angelalbi <angelabi2003@berkeley.edu>
2024-04-05 13:10:03 -0700

    manager.save

--- settings/src/index.ts
+++ settings/src/index.ts
@@ -26,19 +26,36 @@ const extension: JupyterFrontEndPlugin<void> = {
 
     const manager = new SettingManager();
 
-    // async function save(id: string, json: string) {
-    //   await manager.save(id, json);
-    //   console.log(manager);
+    // async function fetch(id: string) {
+    //   try {
+    //     const result_id = await manager.fetch(id);
+    //     console.log("result of manager.fetch", result_id);
+
+    //   } catch (e) {
+    //     console.log('an error occurred while fetching', e)
+    //   }
     // }
 
-    // save(PLUGIN_ID, JSON.stringify(settings))
+    async function fetch_and_save(id:string, settings: ISettingRegistry.ISettings, manager: SettingManager) {
+      try {
+        const fetched_settings = await manager.fetch(id);
+        console.log("result of manager.fetch before save", fetched_settings);
+        console.log("result of settings passed in", settings)
 
-    async function fetch(id: string) {
-      const result_id = await manager.fetch(id);
-      console.log(result_id);
+        // const json_setting = JSON.stringify(fetched_settings);
+        // console.log(json_setting)
+        const test_json = `{"theme": "hi"}`
+        // await manager.save(id, test_json);
+
+        // const raw = `{}`;
+        await manager.save(id, test_json);
+        console.log(JSON.parse((await manager.fetch(id)).raw));
+      } catch (e) {
+        console.log('an error occurred while fetching', e)
+      }
     }
 
-    fetch('@jupyterlab/shortcuts-extension:shortcuts');
+    // fetch('@jupyterlab/shortcuts-extension:shortcuts');
 
     /**
      * Load the settings for this extension
@@ -56,39 +73,56 @@ const extension: JupyterFrontEndPlugin<void> = {
 
     }
 
+    // async function save(id: string, setting: ISettingRegistry.ISettings) {
+    //   try {
+    //     const json_setting = JSON.stringify(setting)
+    //     await manager.save(id, json_setting);
+    //     console.log(manager);
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // }
+
     // Wait for the application to be restored and
     // for the settings for this plugin to be loaded
     Promise.all([app.restored, settings.load('@jupyterlab/shortcuts-extension:shortcuts')])
     .then(([, settings]) => {
       // read the settings
-      let default_shortcuts = (settings.schema.properties!.shortcuts.default!);
-      console.log("default shortcuts", default_shortcuts)
+      // let default_shortcuts = (settings.schema.properties!.shortcuts.default!);
+      let user_shortcuts = (settings.user.shortcuts!);
 
-      const shortcut_names = Object.values(default_shortcuts);
+      // console.log("default shortcuts", default_shortcuts)
+      console.log("settings before changing default shortcuts", settings.user);
 
-      shortcut_names[16].disabled = true;
-      shortcut_names[16].keys = [];
-      console.log("shortcut 16", shortcut_names[16]);
+      // const shortcut_names = Object.values(default_shortcuts);
+      const user_shortcuts_names = structuredClone(Object.values(user_shortcuts));
 
-      shortcut_names[17].disabled = true;
-      shortcut_names[17].keys = [];
-      console.log("shortcut 17", shortcut_names[17]);
+      // shortcut_names[16].disabled = true;
+      // shortcut_names[16].keys = [];
+      // console.log("shortcut 16", shortcut_names[16]);
 
-      shortcut_names[100].disabled = true;
-      shortcut_names[100].keys = [];
-      console.log("shortcut 100", shortcut_names[100]);
+      // shortcut_names[17].disabled = true;
+      // shortcut_names[17].keys = [];
+      // console.log("shortcut 17", shortcut_names[17]);
 
-      shortcut_names[110].disabled = true;
-      shortcut_names[110].keys = [];
-      console.log("shortcut 100", shortcut_names[110]);
+      // shortcut_names[100].disabled = true;
+      // shortcut_names[100].keys = [];
+      // console.log("shortcut 100", shortcut_names[100]);
 
-      console.log("default shortcuts after changing default shortcuts", default_shortcuts)
+      // shortcut_names[110].disabled = true;
+      // shortcut_names[110].keys = [];
+      // console.log("shortcut 100", shortcut_names[110]);
 
-      // settings.changed.connect(() => {
-      //   let shortcuts = settings.get("shortcuts")
-      //   console.log("shortcuts", shortcuts)
-      //   console.log("settings were changed", settings.schema.properties!.shortcuts.default!)
-      // })
+      user_shortcuts_names[101].disabled = true;
+      user_shortcuts_names[101].keys = [];
+
+      user_shortcuts_names[102].disabled = true;
+      user_shortcuts_names[102].keys = [];
+
+      console.log("user shortcuts after change", user_shortcuts_names)
+      console.log("settings after change", settings)
+
+      fetch_and_save('@jupyterlab/shortcuts-extension:shortcuts', settings, manager);
 
     })
     .catch(reason => {
@@ -96,6 +130,19 @@ const extension: JupyterFrontEndPlugin<void> = {
         `Something went wrong when reading the settings.\n${reason}`
       );
     });
+
+    // Promise.all([app.restored, settings.load('@jupyterlab/shortcuts-extension:shortcuts')])
+    // .then(([, settings]) => {
+    //   // read the settings
+      
+    //   console.log("settings after change", settings)
+    //   //save(PLUGIN_ID, JSON.stringify(settings))
+    // })
+    // .catch(reason => {
+    //   console.error(
+    //     `Something went wrong when reading the settings.\n${reason}`
+    //   );
+    // });
 
     Promise.all([app.restored, settings.load(PLUGIN_ID)])
       .then(([, setting]) => {
